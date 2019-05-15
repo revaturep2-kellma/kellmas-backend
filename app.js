c1431fd51e4dd73f010f0d7b0fb28d92a4cda927
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-15 18:23:29 -0500

    Move oidc strategy to file

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const passport = require('passport');
 const config = require('./config');
-const OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+const oidcStrategy = require('./passport/oidc-strategy');
 
 const User = require('./models/user-model');
 
+// Setup passport
 passport.serializeUser((user, done) => {
   done(null, user.oid);
 });
@@ -19,44 +20,7 @@ passport.deserializeUser((oid, done) => {
     .catch(err => done(err));
 });
 
-passport.use(new OIDCStrategy({
-  identityMetadata: config.creds.identityMetadata,
-  clientID: config.creds.clientID,
-  responseType: config.creds.responseType,
-  responseMode: config.creds.responseMode,
-  redirectUrl: config.creds.redirectUrl,
-  allowHttpForRedirectUrl: config.creds.allowHttpForRedirectUrl,
-  clientSecret: config.creds.clientSecret,
-  validateIssuer: config.creds.validateIssuer,
-  isB2C: config.creds.isB2C,
-  issuer: config.creds.issuer,
-  passReqToCallback: config.creds.passReqToCallback,
-  scope: config.creds.scope,
-  loggingLevel: config.creds.loggingLevel,
-  nonceLifetime: config.creds.nonceLifetime,
-  nonceMaxAmount: config.creds.nonceMaxAmount,
-  useCookieInsteadOfSession: config.creds.useCookieInsteadOfSession,
-  cookieEncryptionKeys: config.creds.cookieEncryptionKeys,
-  clockSkew: config.creds.clockSkew,
-},
-((iss, sub, profile, accessToken, refreshToken, done) => {
-  if (!profile.oid) {
-    return done(new Error('No oid found'), null);
-  }
-
-  // Search DB for user. Create user if it does not exist.
-  User.findOne({ where: { oid: profile.oid } })
-    .then(user => {
-      if (user) {
-        return done(null, user);
-      }
-      User.create({ oid: profile.oid })
-        .then(newUser => done(null, newUser))
-        .catch(err => done(err));
-    })
-    .catch(err => done(err));
-})
-));
+passport.use(oidcStrategy);
 
 const app = express();
 
--- passport/oidc-strategy.js
+++ passport/oidc-strategy.js
@@ -1,34 +1,44 @@
 require('dotenv').config();
 const OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
 const User = require('../models/user-model');
+const config = require('../config');
 
-console.log(process.env.CLIENT_ID);
 const oidcStrategy = new OIDCStrategy({
-  identityMetadata: process.env.IDENTITY_METADATA,
-  clientID: process.env.CLIENT_ID,
-  responseType: 'id_token',
-  responseMode: 'form_post',
-  redirectUrl: process.env.REDIRECT_URL,
-  allowHttpForRedirectUrl: true,
-  validateIssuer: false,
-  clientSecret: process.env.CLIENT_SECRET
-}, (iss, sub, profile, accessToken, refreshToken, done) => {
-  // if (!profile.oid) {
-  //   return done(new Error("No oid found"), null);
-  // }
+  identityMetadata: config.creds.identityMetadata,
+  clientID: config.creds.clientID,
+  responseType: config.creds.responseType,
+  responseMode: config.creds.responseMode,
+  redirectUrl: config.creds.redirectUrl,
+  allowHttpForRedirectUrl: config.creds.allowHttpForRedirectUrl,
+  clientSecret: config.creds.clientSecret,
+  validateIssuer: config.creds.validateIssuer,
+  isB2C: config.creds.isB2C,
+  issuer: config.creds.issuer,
+  passReqToCallback: config.creds.passReqToCallback,
+  scope: config.creds.scope,
+  loggingLevel: config.creds.loggingLevel,
+  nonceLifetime: config.creds.nonceLifetime,
+  nonceMaxAmount: config.creds.nonceMaxAmount,
+  useCookieInsteadOfSession: config.creds.useCookieInsteadOfSession,
+  cookieEncryptionKeys: config.creds.cookieEncryptionKeys,
+  clockSkew: config.creds.clockSkew,
+},
+((iss, sub, profile, accessToken, refreshToken, done) => {
+  if (!profile.oid) {
+    return done(new Error('No oid found'), null);
+  }
 
-  // User.findOne({ where: { oid: profile.oid } })
-  //   .then(user => {
-  //     if (user) {
-  //       return done(null, user);
-  //     }
-  //     console.log('hello');
-  //     // "Auto-registration"
-  //     User.create({ oid: profile.oid })
-  //       .then(newUser => done(null, newUser))
-  //       .catch(err => done(err));
-  //   })
-  //   .catch(err => done(err));
-});
+  // Search DB for user. Create user if it does not exist.
+  User.findOne({ where: { oid: profile.oid } })
+    .then(user => {
+      if (user) {
+        return done(null, user);
+      }
+      User.create({ oid: profile.oid })
+        .then(newUser => done(null, newUser))
+        .catch(err => done(err));
+    })
+    .catch(err => done(err));
+}));
 
 module.exports = oidcStrategy;
