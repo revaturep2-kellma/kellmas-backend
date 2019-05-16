f1a1d4dccbff0e064cf1a46b52e3f0bc82f35904
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-15 21:17:17 -0500

    Create router

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const expressSession = require('express-session');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const passport = require('passport');
 
-const config = require('./config');
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
 const bearerStrategy = require('./passport/bearer-strategy');
 const oidcStrategy = require('./passport/oidc-strategy');
-const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
 const User = require('./models/user-model');
+const authRouter = require('./routers/auth-router');
 
 // Setup passport
 passport.serializeUser((user, done) => {
@@ -19,7 +18,6 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((oid, done) => {
-  console.log('oid: ', oid);
   User.findOne({ where: { oid } })
     .then(user => done(null, user))
     .catch(err => done(err));
@@ -35,65 +33,11 @@ app.use(methodOverride());
 app.use(cookieParser());
 
 // set up session middleware
-app.use(expressSession({ secret: 'keyboard cat', resave: true, saveUninitialized: false }));
-
 app.use(bodyParser.urlencoded({ extended : true }));
-
 app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(__dirname + '/../../public'));
 
-app.get('/login',
-  (req, res, next) => {
-    passport.authenticate('azuread-openidconnect',
-      {
-        response: res,                      // required
-        resourceURL: config.resourceURL,    // optional. Provide a value if you want to specify the resource.
-        customState: 'my_state',            // optional. Provide a value if you want to provide custom state value.
-        failureRedirect: '/'
-      }
-    )(req, res, next);
-  },
-  (req, res) => {
-    console.info('Login was called in the Sample');
-    res.redirect('/');
-  });
-
-app.get('/auth/openid/return',
-  (req, res, next) => {
-    passport.authenticate('azuread-openidconnect',
-      {
-        response: res,                      // required
-        failureRedirect: '/'
-      }
-    )(req, res, next);
-  },
-  (req, res) => {
-    console.info('We received a return from AzureAD.');
-    res.redirect('/');
-  });
-
-app.post('/auth/openid/return',
-  (req, res, next) => {
-    passport.authenticate('azuread-openidconnect',
-      {
-        response: res,                      // required
-        failureRedirect: '/'
-      }
-    )(req, res, next);
-  },
-  (req, res) => {
-    console.info('We received a return from AzureAD.');
-    res.redirect(`${CLIENT_ORIGIN}/?token=${req.body.id_token}`);
-  });
-
-// 'logout' route, logout from passport, and destroy the session with AAD.
-app.get('/logout', (req, res) => {
-  req.session.destroy((err) => {
-    req.logOut();
-    res.redirect(config.destroySessionUrl);
-  });
-});
+// Mount routers
+app.use('/auth/openid', authRouter);
 
 // Sample endpoint for authentication
 app.get('/main', passport.authenticate('oauth-bearer', {
---
+++ routers/auth-router.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const passport = require('passport');
+const config = require('../config');
+
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
+const router = express.Router();
+
+router.get('/login',
+  (req, res, next) => {
+    passport.authenticate('azuread-openidconnect',
+      {
+        response: res,                      // required
+        resourceURL: config.resourceURL,    // optional. Provide a value if you want to specify the resource.
+        customState: 'my_state',            // optional. Provide a value if you want to provide custom state value.
+        failureRedirect: '/'
+      }
+    )(req, res, next);
+  },
+  (req, res) => {
+    res.redirect('/');
+  });
+
+router.get('/return',
+  (req, res, next) => {
+    passport.authenticate('azuread-openidconnect',
+      {
+        response: res,                      // required
+        failureRedirect: '/'
+      }
+    )(req, res, next);
+  },
+  (req, res) => {
+    res.redirect('/');
+  });
+
+router.post('/return',
+  (req, res, next) => {
+    passport.authenticate('azuread-openidconnect',
+      {
+        response: res,                      // required
+        failureRedirect: '/'
+      }
+    )(req, res, next);
+  },
+  (req, res) => {
+    res.redirect(`${CLIENT_ORIGIN}/?token=${req.body.id_token}`);
+  });
+
+module.exports = router;
