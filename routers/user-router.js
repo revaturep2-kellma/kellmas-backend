4f4dae6a04b40d0274ef1695383774728c7ec0af
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-16 15:40:54 -0500

    Use userPrincipalName instead of email

--- routers/user-router.js
+++ routers/user-router.js
@@ -3,20 +3,20 @@ const path = require('path');
 const shell = require('shelljs');
 
 const router = express.Router();
-const scriptDir = path.join(__dirname, '../scripts/');
+const scriptDir = path.join(__dirname, '../scripts');
 
 router.post('/', (req, res) => {
-  const { username, password, email } = req.body;
+  const { username, password, userPrincipalName } = req.body;
 
-  if (!username || !password || !email) {
+  if (!username || !password || !userPrincipalName) {
     res.json('missing field');
   }
 
-  shell.exec(`${scriptDir}user.sh ${username} ${password} ${email} `, (code, stdout, stderr) => {
+  shell.exec(`${scriptDir}/user.sh ${username} ${password} ${userPrincipalName} `, (code, stdout, stderr) => {
     if (stderr) {
       res.json('there was a problem');
     } else {
-      res.json(`Account created. Please log in with the email: ${email}`);
+      res.json(`Account created. Please log in with the email: ${userPrincipalName}`);
     }
   });
 
