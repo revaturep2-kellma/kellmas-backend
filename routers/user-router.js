8e6cec6c2e2049f4b8fe473ebd0ae6a351de8028
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-18 17:16:57 -0500

    Revive scripts from the graveyard

--- routers/user-router.js
+++ routers/user-router.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
+const shell = require('shelljs');
 
 const router = express.Router();
+const scriptDir = path.join(__dirname, '../scripts');
 
 router.post('/', (req, res) => {
   const { username, password, userPrincipalName } = req.body;
@@ -9,7 +12,15 @@ router.post('/', (req, res) => {
     return res.json({ error: 'missing field' });
   }
 
-  res.json({ success: 'temp code make account here' });
+
+  shell.exec(`${scriptDir}/user.sh ${username} ${password} ${userPrincipalName} `, (code, stdout, stderr) => {
+    if (stderr) {
+      return res.json({ error: stderr });
+    } else {
+      return res.json({ success: `Account created. Please log in with the email: ${userPrincipalName}` });
+    }
+  });
+
 });
 
 
