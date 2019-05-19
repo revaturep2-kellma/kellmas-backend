9a47f2ccb6292c90f7c84d1b30a83b77d3cafa17
jackson218 <wbjackson218@gmail.com>
2019-05-19 18:50:53 -0500

    updated serverName variable

--- routers/mySQL-router.js
+++ routers/mySQL-router.js
@@ -6,18 +6,18 @@ const router = express.Router();
 const scriptDir = path.join(__dirname, '../scripts');
 
 router.post('/', (req, res) => {
-  const { groupName, serverPassword, serverUser, serverName, dbName } = req.body;
+  const { groupName, serverPassword, serverUserName, serverName, dbName } = req.body;
 
-  if (!groupName || !serverPassword || !serverUser || !serverName || !dbName) {
+  if (!groupName || !serverPassword || !serverUserName || !serverName || !dbName) {
     return res.json({ error: 'missing field' });
   }
 
 
-  shell.exec(`${scriptDir}/mySQL.sh ${groupName} ${serverPassword} ${serverUser} ${dbName} `, (code, stdout, stderr) => {
+  shell.exec(`${scriptDir}/mySQL.sh ${groupName} ${serverPassword} ${serverUserName} ${dbName} `, (code, stdout, stderr) => {
     if (stderr) {
       return res.json({ error: stderr });
     } else {
-      return res.json({ success: `SQL ${serverUser} created` });
+      return res.json({ success: `SQL ${serverUserName} created` });
     }
   });
 
