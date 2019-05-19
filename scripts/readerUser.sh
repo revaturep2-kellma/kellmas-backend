8fc078abd50e530327435258eaf625209634aefc
Matthew Schwartz <Mschw90@gmail.com>
2019-05-18 20:07:06 -0500

    can create new users and also sign up a new user

--- scripts/readerUser.sh
+++ scripts/readerUser.sh
@@ -10,6 +10,10 @@ if [ -n "$userCheck" ]; then
     echo "this user name already exist please choose another"
 fi
 
+echo $displayName
+echo $password
+echo $principalName
+
 az ad user create --display-name $displayName --password $password --user-principal-name $principalName 
 
 az role assignment create --assignee $principalName --role Reader
