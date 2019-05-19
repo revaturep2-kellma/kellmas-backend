b51eaa5f6f43290a9fbf530086ae23c319ee144b
jackson218 <wbjackson218@gmail.com>
2019-05-19 18:19:12 -0500

    updated the user-router

--- routers/user-router.js
+++ routers/user-router.js
@@ -7,25 +7,19 @@ const router = express.Router();
 const scriptDir = path.join(__dirname, '../scripts');
 
 router.post('/', (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, groupName } = req.body;
 
-  if (!username || !password) {
+  if (!username || !password || !groupName) {
     return res.json({ error: 'missing field' });
   }
 
   const userPrincipalName = username + '@' + process.env.AZ_DOMAIN;
 
-  shell.exec(`${scriptDir}/adminUser.sh ${username} ${password} ${userPrincipalName} `, (code, stdout, stderr) => {
+  shell.exec(`${scriptDir}/adminUser.sh ${username} ${password} ${userPrincipalName} ${groupName}`, (code, stdout, stderr) => {
     if (stderr) {
       return res.json({ error: stderr });
     } else {
-      shell.exec(`${scriptDir}/resourceGroup.sh ${username}`, (code, stdout, stderr) => {
-        if (stderr) {
-          return res.json({ error: stderr });
-        } else {
-          return res.json({ success: `Account created. Please log in with the email: ${userPrincipalName}` });
-        }
-      });
+      return res.json({ success: `Account created. Please log in with the email: ${userPrincipalName}` });
     }
   });
 });
