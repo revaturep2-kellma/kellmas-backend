1ceafe5e3172b1faa3386b06be1dff22fbf4dc9d
jackson218 <wbjackson218@gmail.com>
2019-05-21 15:41:04 -0500

    added quotes around all vm.sh variables

--- scripts/vm.sh
+++ scripts/vm.sh
@@ -5,10 +5,10 @@ groupName=$2
 size=$3
 netName=$4
 
-VMcheck=$(az vm list --query [].name | grep -E $vmName)
+VMcheck=$(az vm list --query [].name | grep -E "$vmName")
 
 if [ -n "$VMcheck" ]; then 
     echo "this vm name already exist please choose another"
 fi
 
-az vm create -n $vmName -g $groupName --size $size --vnet-name $netName --image UbuntuLTS --no-wait
+az vm create -n "$vmName" -g "$groupName" --size $size --vnet-name "$netName" --image UbuntuLTS --no-wait
