a82ffd795bcf5d89f2526e58f4d8f539b3b3e81a
jackson218 <wbjackson218@gmail.com>
2019-05-21 15:51:32 -0500

    added quotes around all blobStorage.sh variables

--- scripts/blobStorage.sh
+++ scripts/blobStorage.sh
@@ -5,14 +5,14 @@ storageAccountName=$2
 location=$3
 storagePlan=$4
 
-storageCheck=$(az storage account list --query [].name | grep -E $storageAccountName)
+storageCheck=$(az storage account list --query [].name | grep -E "$storageAccountName")
 
 if [ -n "$storageCheck" ]; then 
     echo "this storage name already exist please choose another"
 fi
 
 # Create a storage account
-az storage account create --resource-group $groupName --name $storageAccountName --location $location --kind blobstorage --sku $storagePlan --access-tier hot --no-wait
+az storage account create --resource-group "$groupName" --name "$storageAccountName" --location "$location" --kind blobstorage --sku "$storagePlan" --access-tier hot --no-wait
 
 # blobStorageAccountKey=$(az storage account keys list -g $groupName \
 # -n $storageAccountName --query [0].value --output tsv)
