e5981fffc76a19ead6ebcb62ddf780e0c970275f
jackson218 <wbjackson218@gmail.com>
2019-05-21 14:57:52 -0500

    changed to servicePlanType

--- routers/webApp-router.js
+++ routers/webApp-router.js
@@ -6,14 +6,14 @@ const router = express.Router();
 const scriptDir = path.join(__dirname, '../scripts');
 
 router.post('/', (req, res) => {
-  const { groupName, servicePlanName, servicePlan,location, webAppName, webAppType, webAppGitRepo } = req.body;
+  const { groupName, servicePlanName, servicePlanType,location, webAppName, webAppType, webAppGitRepo } = req.body;
 
-  if (!groupName || !servicePlanName || !servicePlan || !location || !webAppName || !webAppType || !webAppGitRepo) {
+  if (!groupName || !servicePlanName || !servicePlanType || !location || !webAppName || !webAppType || !webAppGitRepo) {
     return res.json({ error: 'missing field' });
   }
 
 
-  shell.exec(`${scriptDir}/webApp.sh ${groupName} ${servicePlanName} ${servicePlan} ${location} ${webAppName} ${webAppType} ${webAppGitRepo} `, (code, stdout, stderr) => {
+  shell.exec(`${scriptDir}/webApp.sh ${groupName} ${servicePlanName} ${servicePlanType} ${location} ${webAppName} ${webAppType} ${webAppGitRepo} `, (code, stdout, stderr) => {
     if (stderr) {
       return res.json({ error: stderr });
     } else {
