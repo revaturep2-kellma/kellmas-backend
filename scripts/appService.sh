454d3e25a46c3522dd33adea2d6d061657ee40ad
jackson218 <wbjackson218@gmail.com>
2019-05-21 15:53:02 -0500

    added quotes around all appService.sh variables

--- scripts/appService.sh
+++ scripts/appService.sh
@@ -5,11 +5,11 @@ servicePlanName=$2
 servicePlan=$3
 location=$4
 
-planCheck=$(az appservice plan list --query [].name | grep -E $servicePlanName)
+planCheck=$(az appservice plan list --query [].name | grep -E "$servicePlanName")
 
 if [ -n "$planCheck" ]; then 
     echo "this app services name already exist please choose another"
 fi
 
 # Create an App Service 
-az appservice plan create --name $servicePlanName --resource-group $groupName --sku $servicePlan --location $location --is-linux --no-wait
+az appservice plan create --name "$servicePlanName" --resource-group "$groupName" --sku "$servicePlan" --location "$location" --is-linux
