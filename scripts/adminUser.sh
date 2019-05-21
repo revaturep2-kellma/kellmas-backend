6d209bccaf90681f4b518d31bc51a77de51eb632
jackson218 <wbjackson218@gmail.com>
2019-05-21 15:55:18 -0500

    added quotes around all adminUser.sh variables

--- scripts/adminUser.sh
+++ scripts/adminUser.sh
@@ -6,20 +6,20 @@ principalName=$3
 groupName=$4
 location=$5
 
-userCheck=$(az ad user list --query [].name | grep -E $principalName)
+userCheck=$(az ad user list --query [].name | grep -E "$principalName")
 
 if [ -n "$userCheck" ]; then
     echo "this user name already exist please choose another"
 fi
 
-az ad user create --display-name $displayName --password $password --user-principal-name $principalName
+az ad user create --display-name "$displayName" --password "$password" --user-principal-name "$principalName"
 
-az ad group create --display-name $displayName --mail-nickname $displayName
+az ad group create --display-name "$displayName" --mail-nickname "$displayName"
 
-userOID=$(az ad user show --upn-or-object-id $principalName --query objectId -o tsv)
+userOID=$(az ad user show --upn-or-object-id "$principalName" --query objectId -o tsv)
 
-az ad group member add -g $displayName --member-id $userOID
+az ad group member add -g "$displayName" --member-id "$userOID"
 
-az group create --name $groupName --location $location
+az group create --name "$groupName" --location "$location"
 
-az role assignment create --assignee $principalName --role Owner --resource-group $groupName --no-wait
+az role assignment create --assignee "$principalName" --role Owner --resource-group "$groupName" --no-wait
