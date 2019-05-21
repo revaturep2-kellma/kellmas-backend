f5d1ed7959d5d1c63550c289d2a47af21ef95a0f
jackson218 <wbjackson218@gmail.com>
2019-05-21 15:50:12 -0500

    added quotes around all readerUser.sh variables

--- scripts/readerUser.sh
+++ scripts/readerUser.sh
@@ -5,16 +5,16 @@ password=$2
 principalName=$3
 groupName=$4
 
-userCheck=$(az ad user list --query [].name | grep -E $principalName)
+userCheck=$(az ad user list --query [].name | grep -E "$principalName")
 
 if [ -n "$userCheck" ]; then 
     echo "this user name already exist please choose another"
 fi
 
-echo $displayName
-echo $password
-echo $principalName
+echo "$displayName"
+echo "$password"
+echo "$principalName"
 
-az ad user create --display-name $displayName --password $password --user-principal-name $principalName 
+az ad user create --display-name "$displayName" --password "$password" --user-principal-name "$principalName" 
 
-az role assignment create --assignee $principalName --role Reader --resource-group $groupName --no-wait
+az role assignment create --assignee "$principalName" --role Reader --resource-group "$groupName" --no-wait
