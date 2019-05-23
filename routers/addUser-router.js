a74b74103d4c04401f60d0cc085c8231db4a98f3
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-22 20:50:12 -0500

    Update addUser-router.js

--- routers/addUser-router.js
+++ routers/addUser-router.js
@@ -7,23 +7,35 @@ const router = express.Router();
 const scriptDir = path.join(__dirname, '../scripts');
 
 router.post('/', (req, res) => {
-  const { username, password, groupName } = req.body;
-
-  if (!username || !password || !groupName) {
-    return res.json({ error: 'missing field' });
-  }
-
+  const { username, password } = req.body;
   const userPrincipalName = username + '@' + process.env.AZ_DOMAIN;
 
-  console.log(password);
-  shell.exec(`${scriptDir}/readerUser.sh "${username}" "${password}" "${userPrincipalName}" "${groupName}"`, (code, stdout, stderr) => {
+  if (!username) {
+    return res.json({ error: 'missing field `username`' });
+  }
+
+  if (!password) {
+    return res.json({ error: 'missing field `password`' });
+  }
+
+  shell.exec(`${scriptDir}/findGroup.sh "${req.user.oid}"`, (code, stdout, stderr) => {
     if (stderr) {
       return res.json({ error: stderr });
     } else {
-      return res.json({ success: `User ${userPrincipalName} created` });
+      stdout = JSON.parse(stdout);
+      if (!stdout || stdout.role !== 'Owner') {
+        return res.json({ error: 'Insufficient privilege. Must be owner to create a new user.' });
+      }
+
+      shell.exec(`${scriptDir}/readerUser.sh "${username}" "${password}" "${userPrincipalName}" "${stdout.groupName}"`, (code, stdout, stderr) => {
+        if (stderr) {
+          return res.json({ error: stderr });
+        } else {
+          return res.json({ success: `User ${userPrincipalName} created` });
+        }
+      });
     }
   });
-
 });
 
 
