9c306d5205b58b83b34f466def0cc2c996bde4b8
jackson218 <wbjackson218@gmail.com>
2019-05-23 21:14:05 -0500

    added NSG for vnet.sh

--- scripts/vnet.sh
+++ scripts/vnet.sh
@@ -3,6 +3,8 @@
 groupName=$1
 netName=$2
 location=$3
+nsgName=$4
+
 
 vnetCheck=$(az network vnet list --query [].name | grep -E "$netName")
 
@@ -12,3 +14,4 @@ fi
 
 az network vnet create -g "$groupName" -n "$netName" --location "$location"
 
+az network nsg rule create -g $groupName --nsg-name $nsgName -n PrivateNetwork --priority 400 --source-address-prefixes VirtualNetwork --destination-port-ranges 80,443 --direction Inbound --access Allow --protocol TCP 
