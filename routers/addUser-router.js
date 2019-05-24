022fc57f06d6efc30a02f212b37f865b485f96d0
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-23 21:15:58 -0500

    Create route for getting users

--- routers/addUser-router.js
+++ routers/addUser-router.js
@@ -6,6 +6,28 @@ const shell = require('shelljs');
 const router = express.Router();
 const scriptDir = path.join(__dirname, '../scripts');
 
+router.get('/', (req, res) => {
+  shell.exec(`${scriptDir}/findGroup.sh "${req.user.oid}"`, (code, stdout, stderr) => {
+    if (stderr) {
+      return res.json({ error: stderr });
+    } else {
+      stdout = JSON.parse(stdout);
+      if (!stdout || stdout.role !== 'Owner') {
+        return res.json({ error: 'Insufficient privilege. Must be owner to create a new user.' });
+      }
+
+      shell.exec(`${scriptDir}/showUsers.sh "${stdout.resourceGroup}"`, (code, stdout, stderr) => {
+        console.log('===============================================');
+        if (stderr) {
+          return res.json({ error: stderr });
+        } else {
+          return res.json(JSON.parse(stdout));
+        }
+      });
+    }
+  });
+});
+
 router.post('/', (req, res) => {
   const { username, password } = req.body;
   const userPrincipalName = username + '@' + process.env.AZ_DOMAIN;
